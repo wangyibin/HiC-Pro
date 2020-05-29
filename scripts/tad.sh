2b59bbcab79f9acb121b9f7e43aba45a0d00f489
Yibin Wang <738339463@qq.com>
2020-05-29 10:23:42 +0800

    change tad.sh generate new resolution log

--- scripts/tad.sh
+++ scripts/tad.sh
@@ -86,7 +86,7 @@ do
         echo "Logs: ${ldir}/tad_${bsize}.log"
         if [[ ! -e ${DATA_DIR}/${RES_FILE_NAME}/raw/${bsize}/ || ! -e ${DATA_DIR}/${RES_FILE_NAME}/iced/${bsize}/ ]]; then
             cmd="generate_new_resolution.py -i ${MAPC_OUTPUT}/data/${RES_FILE_NAME}/${RES_FILE_NAME}.allValidPairs -b ${bsize} -c ${GENOME_SIZE_FILE} -o ${DATA_DIR}/${RES_FILE_NAME}/ "
-            exec_cmd ${cmd} >> ${ldir}/heatmap_${bsize}.log 2>&1
+            exec_cmd ${cmd} >> ${ldir}/tad_${bsize}.log 2>&1
         fi
 
         abs_bed=$(find -L ${DATA_DIR}/${RES_FILE_NAME}/raw/${bsize}/ -name "*_${bsize}_abs.bed")
@@ -96,7 +96,7 @@ do
         #ln -s ${iced_matrix} ${RES_DIR}/
         if [[ ! -z $abs_bed || ! -z $iced_matrix ]]; then
             cmd="generate_new_resolution.py -i ${MAPC_OUTPUT}/data/${RES_FILE_NAME}/${RES_FILE_NAME}.allValidPairs -b ${bsize} -c ${GENOME_SIZE_FILE} -o ${DATA_DIR}/${RES_FILE_NAME}/ "
-            exec_cmd ${cmd} >> ${ldir}/heatmap_${bsize}.log 2>&1
+            exec_cmd ${cmd} >> ${ldir}/tad_${bsize}.log 2>&1
 
         fi
         abs_bed=$(find -L ${DATA_DIR}/${RES_FILE_NAME}/raw/${bsize}/ -name "*_${bsize}_abs.bed")
