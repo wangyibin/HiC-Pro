8d7b91a0487deec4574f5cf699f89a00d8265b72
Yibin Wang <738339463@qq.com>
2020-05-29 09:08:20 +0800

    change qc validStat outdir to qc_dir

--- scripts/qc.sh
+++ scripts/qc.sh
@@ -67,10 +67,10 @@ do
     echo "Logs: ${ldir}/qc.log"
 
     if [ -d ${DATA_DIR}/${RES_FILE_NAME} ]; then
-        cmd="python -m TDGP.analysis.qc validStat ${MAPC_OUTPUT}/stats/${RES_FILE_NAME}/ hicvalidPairs.stat -f 2 "
+        cmd="python -m TDGP.analysis.qc validStat ${MAPC_OUTPUT}/stats/${RES_FILE_NAME}/ ${QC_DIR}/${RES_FILE_NAME}/hicvalidPairs.stat -f 2 "
         exec_cmd $cmd >> ${ldir}/qc.log 2>&1
 
-        cmd="estimate_hic_resolution.py ${DATA_DIR}/${RES_FILE_NAME}/${RES_FILE_NAME}.allValidPairs ${GENOME_SIZE} -o ${QC_DIR}/${RES_FILE_NAME} -t ${N_CPU}"
+        cmd="estimate_hic_resolution.py ${DATA_DIR}/${RES_FILE_NAME}/${RES_FILE_NAME}.allValidPairs ${GENOME_SIZE} -o ${QC_DIR}/${RES_FILE_NAME}/${RES_FILE_NAME}.allValidPairs -t ${N_CPU}"
         exec_cmd $cmd >> ${ldir}/qc.log 2>&1
         
         
